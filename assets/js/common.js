05345882e85c2e5b7ce15d82134cd4350a9606d1
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-01-16 00:02:48 +0000

    added google scholar id [ci skip]

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,16 +1,1 @@
-$(document).ready(function() {
-    $('a.abstract').click(function() {
-        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
-    });
-    $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
-    });
-    $('a').removeClass('waves-effect waves-light');
-});
-
-$(document).ready(function() {
-  $('.grid').masonry({
-    itemSelector: '.grid-item',
-    isFitWidth: true
-  });
-});
+$(document).ready(function(){$("a.abstract").click(function(){$(this).parent().parent().find(".abstract.hidden").toggleClass("open")}),$("a.bibtex").click(function(){$(this).parent().parent().find(".bibtex.hidden").toggleClass("open")}),$("a").removeClass("waves-effect waves-light")}),$(document).ready(function(){$(".grid").masonry({itemSelector:".grid-item",isFitWidth:!0})});
